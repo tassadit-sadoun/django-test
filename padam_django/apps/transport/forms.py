9021c8a62a0df8af23b7b0a42eee12baa72d8e70
TSAD01 <you@example.com>
2025-12-10 03:00:06 +0100

    Refactor BusShiftForm to remove unused methods and clean up code

--- padam_django/apps/transport/forms.py
+++ padam_django/apps/transport/forms.py
@@ -1,18 +1,9 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from .models import BusShift
 
 class BusShiftForm(forms.ModelForm):
-    inline_formset = None  # sera injecté depuis l'admin
+    inline_formset = None
 
     class Meta:
         model = BusShift
         fields = ['bus', 'driver', 'status']
-
-    def set_inline_formset(self, formset):
-        """Injecte l'inline formset depuis l'admin."""
-        self.inline_formset = formset
-
-    def clean(self):
-        return super().clean()
-
